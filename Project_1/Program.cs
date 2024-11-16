5b1422efb0a7da9c85c0d53988afa539db0430ca
SHLAPAK MICHAEL RODIONOVICH <misha.shlapak921@gmail.com>
2024-11-16 19:06:42 +0700

    V 1.0. Добавлен базовый функционал

--- Project_1/Program.cs
+++ Project_1/Program.cs
@@ -1,5 +1,81 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace TextQuest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Добро пожаловать в текстовый квест!");
+            Console.WriteLine("Вы просыпаетесь в темной комнате. Как вы будете действовать?");
+            Console.WriteLine("1. Осмотреться.");
+            Console.WriteLine("2. Выйти из комнаты.");
+            Console.Write("Введите номер действия: ");
+
+            string choice = Console.ReadLine();
+
+            if (choice == "1")
+            {
+                InspectRoom();
+            }
+            else if (choice == "2")
+            {
+                ExitRoom();
+            }
+            else
+            {
+                Console.WriteLine("Неверный выбор, попробуйте еще раз.");
+            }
+        }
+
+        static void InspectRoom()
+        {
+            Console.WriteLine("Вы осматриваетесь и находите старый ключ.");
+            Console.WriteLine("Теперь вы можете:n1. Взять ключ.n2. Выйти из комнаты.");
+            Console.Write("Введите номер действия: ");
+
+            string choice = Console.ReadLine();
+
+            if (choice == "1")
+            {
+                Console.WriteLine("Вы взяли ключ. Он может пригодиться вам позже.");
+                // Здесь можно добавить логику с использованием ключа.
+            }
+            else if (choice == "2")
+            {
+                ExitRoom();
+            }
+            else
+            {
+                Console.WriteLine("Неверный выбор, попробуйте еще раз.");
+                InspectRoom();
+            }
+        }
+
+        static void ExitRoom()
+        {
+            Console.WriteLine("Вы выходите из комнаты и попадаете в длинный коридор.");
+            Console.WriteLine("На вашем пути есть две двери.");
+            Console.WriteLine("1. Дверь слева.n2. Дверь справа.");
+            Console.Write("Введите номер действия: ");
+
+            string choice = Console.ReadLine();
+
+            if (choice == "1")
+            {
+                Console.WriteLine("Вы входите в комнату с сокровищами! Вы победили!");
+                // Можно добавить дополнительную логику, например, возможность забрать сокровища.
+            }
+            else if (choice == "2")
+            {
+                Console.WriteLine("Вы входите в комнату с монстрами и проигрываете!");
+                // Можно добавить возможность начать игру заново.
+            }
+            else
+            {
+                Console.WriteLine("Неверный выбор, попробуйте еще раз.");
+                ExitRoom();
+            }
+        }
+    }
+}
